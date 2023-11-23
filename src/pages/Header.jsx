ef408fd9fe161ebc427f27c3f5487cd675915308
Abhishek Bhattacharjee <ahishek1250@gmail.com>
2023-11-23 22:33:36 +0530

    feat: Added hamburger menu and added respective styles for each screen size

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,32 +1,73 @@
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { IoCloseSharp } from "react-icons/io5";
+
 export default function Header() {
+  const [isOpenNav, setIsOpenNav] = useState(false);
+
   return (
-    <section className="section-header">
-      <div className="logo">
-        <img src="./logo.svg" alt="website logo" />
-      </div>
+    <>
+      <section className="section-header">
+        <div className="logo">
+          <img src="./logo.svg" alt="website logo" />
+        </div>
 
-      <nav className="nav">
-        <ul className="nav-list">
-          <li className="nav-item">
-            <a href="#">Features</a>
-          </li>
-          <li className="nav-item">
-            <a href="#">Pricing</a>
-          </li>
-          <li className="nav-item">
-            <a href="#">Resources</a>
-          </li>
-        </ul>
-      </nav>
+        <nav className="nav">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <a href="#">Features</a>
+            </li>
+            <li className="nav-item">
+              <a href="#">Pricing</a>
+            </li>
+            <li className="nav-item">
+              <a href="#">Resources</a>
+            </li>
+          </ul>
+        </nav>
 
-      <div className="auth">
-        <a href="#" className="auth-login">
-          Login
-        </a>
-        <a href="#" className="auth-signup">
-          Sign Up
-        </a>
-      </div>
-    </section>
+        <div className="auth">
+          <a href="#" className="auth-login">
+            Login
+          </a>
+          <a href="#" className="auth-signup">
+            Sign Up
+          </a>
+        </div>
+
+        <div
+          className="hamburger-menu"
+          onClick={() => setIsOpenNav((isOpenNav) => !isOpenNav)}
+        >
+          {isOpenNav ? <IoCloseSharp /> : <FaBars />}
+        </div>
+      </section>
+
+      {isOpenNav && (
+        <div className="dropdown-menu">
+          <nav className="dropdown-nav">
+            <ul className="nav-list">
+              <li className="nav-item">
+                <a href="#">Features</a>
+              </li>
+              <li className="nav-item">
+                <a href="#">Pricing</a>
+              </li>
+              <li className="nav-item">
+                <a href="#">Resources</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="dropdown-auth">
+            <a href="#" className="auth-login">
+              Login
+            </a>
+            <a href="#" className="auth-signup">
+              Sign Up
+            </a>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
