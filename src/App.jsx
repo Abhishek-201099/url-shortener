410056e744c93f71dfe8c9f0e8f69992c550cecd
Abhishek Bhattacharjee <ahishek1250@gmail.com>
2023-11-29 17:29:20 +0530

    Added CTA section and added styling for responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Cta from "./components/Cta";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Shortener from "./components/Shortener";
@@ -11,6 +12,7 @@ export default function App() {
         <Hero />
         <Shortener />
         <Stats />
+        <Cta />
       </main>
     </div>
   );
---
+++ src/components/Cta.jsx
@@ -0,0 +1,10 @@
+export default function Cta() {
+  return (
+    <section className="section-cta">
+      <div className="cta-container">
+        <h2 className="cta-heading">Boost your links today</h2>
+        <button className="cta-btn">Get Started</button>
+      </div>
+    </section>
+  );
+}
